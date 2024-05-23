2f99fddd8ed04852643058322bd98773274eb0e5
AshleyIScott <159094007+Ashis13@users.noreply.github.com>
2024-05-22 20:04:52 -0700

    Bootstrap added
    * updated footer links to link to this project for github & netlify
    * added bootstrap 5.3

--- src/App.js
+++ src/App.js
@@ -3,37 +3,39 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> weather app 🌤</h1>
+      <div className="container">
+        <h1> weather app 🌤</h1>
 
-      <footer>
-        coded by
-        <a
-          href="https://github.com/AshleyIScott"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Ashley Irving-Scott{" "}
-        </a>
-        ◇ open-sourced on
-        <a
-          href="https://github.com/AshleyIScott/ReactWeatherApp"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          GitHub{" "}
-        </a>
-        ◇ hosted on
-        <a
-          href="https://main--dashing-trifle-8edf30.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Netlify
-        </a>
-      </footer>
+        <footer>
+          coded by
+          <a
+            href="https://github.com/AshleyIScott"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Ashley Irving-Scott{" "}
+          </a>
+          ◇ open-sourced on
+          <a
+            href="https://github.com/AshleyIScott/week5-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            GitHub{" "}
+          </a>
+          ◇ hosted on
+          <a
+            href=" https://master--preeminent-maamoul-7886b8.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
