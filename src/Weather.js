31b8edf6e910a34411192f4aa8ced293f5900146
AshleyIScott <159094007+Ashis13@users.noreply.github.com>
2024-05-24 18:22:42 -0700

    button colour update
    * updated the colour of the search button

--- src/Weather.js
+++ src/Weather.js
@@ -51,11 +51,7 @@ export default function Weather(props) {
               />
             </div>
             <div className="col-3">
-              <input
-                type="submit"
-                value="search"
-                className="btn btn-primary w-100"
-              />
+              <input type="submit" value="search" className="btn w-100" />
             </div>
           </div>
         </form>
