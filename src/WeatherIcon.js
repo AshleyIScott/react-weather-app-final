f52349430fb14c362165fd6a5755492fba677ef6
AshleyIScott <159094007+Ashis13@users.noreply.github.com>
2024-05-27 19:29:23 -0700

    initial build for Forecast
    * created new component for Forecast (one day only)
    * created css file for styling forecast; styled forecast
    * adjusted WeatherIcon size to be a property

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#24527a"
-      size={72}
+      size={props.size}
       animate={true}
     />
   );
