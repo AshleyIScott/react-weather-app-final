d8ba7332375dcff98ef8261325e86bba42a67d4b
AshleyIScott <159094007+Ashis13@users.noreply.github.com>
2024-05-28 18:41:38 -0700

    completed component for WeatherForecastDay
    * completed work on the component for formatting the day in the forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -3,21 +3,33 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
+  function maxTemp() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+  function minTemp() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
     <div>
-      <div className="WeatherForecastDay">{props.data.dt}</div>
+      <div className="WeatherForecastDay">{day()}</div>
       <div>
         <WeatherIcon code={props.data.weather[0].icon} size={33} />
       </div>{" "}
       <div className="WeatherForecastTemps">
-        <span className="WeatherForecastTemp-max">
-          {props.data.temp.max}°C{" "}
-        </span>{" "}
-        |
-        <span className="WeatherForecastTemp-min">
-          {" "}
-          {props.data.temp.min}°C{" "}
-        </span>
+        <span className="WeatherForecastTemp-max">{maxTemp()} </span> |
+        <span className="WeatherForecastTemp-min"> {minTemp()} </span>
       </div>
     </div>
   );
